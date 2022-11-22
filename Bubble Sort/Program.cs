4095497449273d348b5e4472e421c153ff06a918
Joao Roas <jr.roas@gmail.com>
2022-11-21 21:11:54 -0300

    Resolvendo Bubble Sorte

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,19 @@
+﻿var nums = new int[15]
+{
+    3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48
+};
+
+
+Console.WriteLine("Array desordenado");
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"{nums[i]}, ");
+}
+
+
+Console.WriteLine("Array ordenado com bubble sort");
+for (int i = 0; i < nums.Length; i++)
+{
+    
+}
+
