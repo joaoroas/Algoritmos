97da0ac0ea43509eb3a079381ece6752ecaefa51
Joao Roas <jr.roas@gmail.com>
2022-11-29 17:57:32 -0300

    Ordenando array

--- Bubble Sort/Program.cs
+++ Bubble Sort/Program.cs
@@ -1,19 +1,42 @@
-﻿var nums = new int[15]
+﻿int[] nums = new int[15]
 {
     3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48
 };
 
 
 Console.WriteLine("Array desordenado");
-for (int i = 0; i < nums.Length; i++)
-{
-    Console.Write($"{nums[i]}, ");
-}
-
+ImprimirArray(nums);
 
 Console.WriteLine("Array ordenado com bubble sort");
-for (int i = 0; i < nums.Length; i++)
+OdernarArrayBubbleSort(ref nums);
+ImprimirArray(nums);
+
+static void OdernarArrayBubbleSort(ref int[] array)
 {
-    
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+
+    }
+
 }
 
+static void ImprimirArray(int[] nums)
+{
+    for (var i = 0; i < nums.Length; i++)
+    {
+        Console.Write($"{nums[i]}, ");
+
+    }
+    Console.WriteLine();
+
+}
